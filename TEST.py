846cbb871a728ce03ccc4fd7c4d1b6071237de88
A1batros19 <konstantin28sat@yandex.ru>
2022-11-24 22:12:17 +0300

    24.11.2022 evening

--- TEST.py
+++ TEST.py
@@ -1,13 +1,5 @@
 a1 = input()
-b1 = input()
-c1 = input()
-a = len(a1)
-b = len(b1)
-c = len(c1)
-min1=min(a,b,c)
-max1=max(a,b,c)
-mid1=(a+b+c)-(min1+max1)
-if mid1-min1==max1-mid1:
+if '@' in(a1) and '.'in(a1) :
     print('YES')
 else:
     print('NO')
