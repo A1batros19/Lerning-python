4fbb96ac737b9f2f6b368edf7a6e3152c9f3c280
A1batros19 <konstantin28sat@yandex.ru>
2022-11-25 21:33:56 +0300

    25.11.2022 operator IN modul MATH

--- TEST.py
+++ TEST.py
@@ -1,6 +1,6 @@
-a1 = input()
-if '@' in(a1) and '.'in(a1) :
-    print('YES')
-else:
-    print('NO')
-
+x=float(input())
+from math import *
+pol = floor(x)
+potolok = ceil(x)
+x1= potolok+pol
+print(x1)
